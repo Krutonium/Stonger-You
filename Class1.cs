bac3417997a5ba15c8dba75b72385f96a9252d01
Krutonium <PFCKrutonium@gmail.com>
2020-12-28 21:02:46 -0500

    Initial Commit

---
+++ Class1.cs
@@ -0,0 +1,94 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace StrongerYou
+{
+    public class ModEntry : Mod
+    {
+        private ModConfig Config;
+        public override void Entry(IModHelper helper)
+        {
+            Config = this.Helper.ReadConfig<ModConfig>();
+            if (Config.Enabled)
+            {
+                helper.Events.GameLoop.DayEnding += GameLoopOnDayEnding;
+                helper.Events.GameLoop.DayStarted += GameLoopOnDayStarted;   
+                Monitor.Log("Enabled with a Multiplier of " + Config.Multiplier, LogLevel.Info);
+            }
+            else
+            {
+                Monitor.Log("Disabled.", LogLevel.Info);   
+            }
+        }
+
+        static int HealthOffset = 50; 
+        static int EnergyOffset = 50;
+        
+        private void CalculateOffsets()
+        {
+            if (!Context.IsWorldReady)
+            {
+                HealthOffset = 0;
+                EnergyOffset = 0;
+                Monitor.Log("Game wasn't ready to calculate offsets.", LogLevel.Error);
+                return;
+            }
+
+            int FarmingLevel = Game1.player.FarmingLevel;
+            int MiningLevel = Game1.player.MiningLevel;
+            int ForagingLevel = Game1.player.ForagingLevel;
+            int FishingLevel = Game1.player.FishingLevel;
+            int CombatLevel = Game1.player.CombatLevel;
+
+            int Bonus = ((FarmingLevel + MiningLevel + ForagingLevel + FishingLevel + CombatLevel) * Config.Multiplier) + Config.BaseExtra;
+            
+            HealthOffset = EnergyOffset = Bonus;
+            Monitor.Log("Bonus Offset is " + Bonus, LogLevel.Info);
+        }
+        
+        private void GameLoopOnDayStarted(object sender, DayStartedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+            {
+                return;
+            }
+            CalculateOffsets();
+            //Add Health and Energy
+            Monitor.Log("Current Health is " + Game1.player.maxHealth, LogLevel.Info);
+            Monitor.Log("Current Energy is "+ Game1.player.maxStamina, LogLevel.Info);
+            Game1.player.maxHealth += HealthOffset;
+            Game1.player.health += HealthOffset;
+            Game1.player.MaxStamina += EnergyOffset;
+            Game1.player.stamina += EnergyOffset;
+            Monitor.Log("Added Bonus Health and Energy based on Levels.", LogLevel.Info);
+        }
+
+        private void GameLoopOnDayEnding(object sender, DayEndingEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+            {
+                return;
+            }
+            //Remove Health and Energy
+            Monitor.Log("Removing Bonus Health and Energy Prior to Save...", LogLevel.Info);
+            Game1.player.maxHealth -= HealthOffset;
+            Game1.player.MaxStamina -= EnergyOffset;
+        }
+    }
+
+    class ModConfig
+    {
+        public bool Enabled = true;
+        public int Multiplier = 2;
+        public int BaseExtra = 0;
+    }
+}
